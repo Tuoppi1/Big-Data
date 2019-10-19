b76e175d74caadcd68498cb0038c2270f8890fae
Tuomas Leinonen <43103458+Tuoppi1@users.noreply.github.com>
2019-10-20 02:24:32 +0300

    Create WordCount_mrjob.py
    Calculates how many times different words occur in a large txt file using mrjob MapReduce.

---
+++ WordCount_mrjob.py
@@ -0,0 +1,18 @@
+from mrjob.job import MRJob
+import re
+pattern = re.compile(r"[\w']+")
+
+class wordCount(MRJob):
+    
+    def mapper(self, _, line):
+        for word in pattern.findall(line):
+            yield word.lower(), 1
+
+    def combiner(self, key, values):
+        yield key, sum(values)
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    wordCount.run()
